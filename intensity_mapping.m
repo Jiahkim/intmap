1e961d33751e151cfd3ff7fdddf3326b2fb290e1
Jiahkim <46355801+Jiahkim@users.noreply.github.com>
2025-12-15 15:59:37 -0500

    intensity_mapping.m

--- intensity_mapping.m
+++ intensity_mapping.m
@@ -1,101 +1,35 @@
-%%%%%%%%%%% for single image
+%This is to averaging time frames.
 clear
 fdir='file directory';
+fname='file name';
 
-fname=['filename'];
-%MAX_MAX_c-HCTGSHDJFX554_ctrl_2Andor13z_1.67sfo5min_1_MMStack_Default.ome_cmle.ome-1
-%MAX_MAX_e-HCTGSHDJFX554_dATP1hr18m_2Andor13z_1.67sfo5min_1-1
-
-clear I0
-I0=double(imread([ fdir fname '.tif'],1 )); %modify image format
-
-I=imgaussfilt(I0,1);
-Ir=rescale(I)*10; %%Intensity is now between 0~10.
-l=0.; h=l+4; %%%Adjust numbers low level, range to show map .
- high=num2str(h); low=num2str(l);
-            Ir(Ir==h|Ir>h)=h;
-            Ir(Ir<l)=l; 
-            r=round(rescale(Ir)*10);
-            
-        % Create colormap 
-        
-        cmap = zeros(10, 3);
-
-  cmap = [1,1,1;...   % wfor 1
-    0.9, 0.9, 0.9; ...       % gr for 2
-    0, 0.7, 1; ...  %1, 0.2, 0.8; ... pink
-    1, 0.4, 1; ...
-    % 0.5,0.4, 1; ...   % light purple for 5
-    1, 0.9, 0; ...       % y =
-    0, 0.5, 0; ...       % green for 6
-    0, 0.6, 0; ...       % green for 7
-    0, 0.7, 0; ...       % green for 8
-    0, 1, 0; ...       % bright green for 9
-    0., 0.2, 0.]   ;        % dark green for 10
-   colormap(cmap)
-     % Display the image with the colormap applied.
-        rgbImage = ind2rgb(r, cmap);
-       figure, imshow(r, cmap);
-
+%define below
+totalframenumb=50; % total 
+timewindow=3; %number of time frame for averaging 
+interval = 1.7; % time-lapse interval
+l=0.; h=l+4;%3.5; % low/high level range to assign colors.
 %%
-       %%%%%%%%%%% for single image
-
-howmanyfr=80;
-for i=1:howmanyfr
-I0=double(imread([ fdir fname ''],i));
-
-I=imgaussfilt(I0,1);
-Ir=rescale(I)*10;%
-Ir=rescale(I)*10;
-l=0; h=l+3;
- high=num2str(h); low=num2str(l);
-            Ir(Ir==h|Ir>h)=h;
-            Ir(Ir<l)=l; 
-            r=round(rescale(Ir)*10);
-        % Create colormap 
-        
-        cmap = zeros(10, 3);
-   
-    cmap = [1,1,1;...  
-    0.9, 0.9, 0.9; ...       
-    0, 0.7, 1; ...  
-    1, 0.4, 1; ...
-    1, 0.9, 0; ...       
-    0, 0.5, 0; ...       
-    0, 0.6, 0; ...       
-    0, 0.7, 0; ...       
-    0, 1, 0; ...       
-    0., 0.2, 0.]   ;        
-      
-      colormap(cmap)
-     % Display the image with the colormap applied.
-%         figure,imshow(r, cmap);
-       rgbImage = ind2rgb(r, cmap);
-       imshow(r, cmap);
-
-end
-%%
-%%%%%%%%%%% for adding frames
-
-fname=['your file'];
-
-%define
-totalframenumb=50; % number of frame
 images = cell(totalframenumb,1); 
-
 for i=1:totalframenumb %i=10
-    I00{i}=double(imread([ fdir fname '.tif'],i)); %modify image formate
-    I0{i}=imgaussfilt(I00{i},0.5);
+    I00{i}=double(imread([fdir fname '.tif'],i )).^0.85;
+   % I0{i}=imgaussfilt(I00{i});
 end
 
-
-  Isum=zeros(size(I0{1}),'double');
-for ii=1:totalframenumb
-    Isum=Isum+I0{(totalframenumb-(totalframenumb-ii))}; %50-(50-k)
-    %%%%%%setting range to be mapped
+for ii=floor(timewindow/2)+1%:totalframenumb-floor(timewindow/2)-1
+ Isum=zeros(size(I00{1}),'double');
+ tosum=I00(ii-floor(timewindow/2):ii+floor(timewindow/2));
+   for iii=1:timewindow
+       Isum=(Isum+tosum{iii}); %50-(50-k)
+   end
+   %%%%%%setting range to be mapped
     %I=imgaussfilt(I0,1);
-    Ir=rescale(Isum)*10;
-    l=0.; h=l+3;%3.5; 
+    Ir0=rescale(imgaussfilt(Isum,1))*10;
+      if ii ==floor(timewindow/2)+1
+          ref = Ir0;
+      end
+      Ir= Ir0*mean(ref,"all")/mean(Ir0,"all");
+
+
     high=num2str(h); low=num2str(l);
      
     Ir(Ir==h|Ir>h)=h;
@@ -106,23 +40,44 @@ for ii=1:totalframenumb
          %   r=round(rescale(Isum)*10);
         %    
             cmap = zeros(10, 3);
-  
-          cmap = [1,1,1;...   
-        0.9, 0.9, 0.9; ...      
-        0, 0.7, 1; ...  
+      %BPY
+          cmap = [1,1,1;...   % wfor 1
+        0.9, 0.9, 0.9; ...       % gr for 2
+        0, 0.7, 1; ...  %1, 0.2, 0.8; ... pink
         1, 0.4, 1; ...
-        1, 0.9, 0; ...      
-        0, 0.5, 0; ...       
-        0, 0.6, 0; ...       
-        0, 0.7, 0; ...       
-        0, 1, 0; ...       
-        0., 0.2, 0.]   ;        
-         % Display the image with the colormap applied.
-             %figure,
-       imshow(r, cmap);
+        % 0.5,0.4, 1; ...   % light purple for 5
+        1, 0.9, 0; ...       % y =
+        0, 0.5, 0; ...       % green for 6
+        0, 0.6, 0; ...       % green for 7
+        0, 0.7, 0; ...       % green for 8
+        0, 1, 0; ...       % bright green for 9
+        0., 0.2, 0.]   ;        % dark green for 10
+         % % Display the image with the colormap applied.
+         %    figure,
+         %       imshow(r, cmap);
+           %     pause(1)
+              % % 
+rgbImage = ind2rgb(r, cmap);
+           % images{ii} = rgbImage;
+result=[rescale(cat(3,  Isum, Isum,  Isum)),rgbImage];
+           % images{ii} = result;
+           
+%%Activate section below to save intensity map
+%  Res = insertText(result, [0 0], [num2str(ii*interval) '±' num2str(floor(timewindow/2*interval)) 'sec'], 'AnchorPoint','LeftTop','TextColor' , 'white', 'fontsize', 10, 'BoxOpacity',0 );
+%  %Res(163:164, 301:309,:)=255; % 9pix for scale bar.
+%  Res(17:19, 3:14,:)=255; % 9pix for scale bar.
+%  Res = insertText(Res, [0 19], '1um', 'AnchorPoint','LeftTop','TextColor' , 'white', 'fontsize', 10, 'BoxOpacity',0 );
+%  images1{ii} = Res;
+%     if ~exist([fdir 'sum' num2str(timewindow) 'frames' ])      
+%     mkdir([fdir fname 'sum' num2str(timewindow) 'frames' ])   
+%     end
 
-        rgbImage = ind2rgb(r, cmap);
-       % images{ii} = rgbImage;
-        result=[rescale(cat(3,  Isum, Isum,  Isum)),rgbImage];
-        images{ii} = result;
+%  imwrite(Res, [fdir  fname 'sum' num2str(timewindow) 'frames/' fname 'T' num2str(timewindow) 'sec_h' high 'l' low 'fr' num2str(ii) '.tif'], 'Resolution', 400);  
+% % 
+%imwrite(result, [fdir  fname 'sum' num2str(timewindow) 'frames/' fname 'T' num2str(timewindow) 'sec_h' high 'l' low 'fr' num2str(ii) '.tif'], 'Resolution', 400);  
+% %     % f=gcf; exportgraphics(f,[fdir fname 'SON_h' high 'l' low 'fr' num2str(i) '.pdf'],'ContentType','vector',...
+% %     %              'BackgroundColor','none','Resolution',400)
+
+
+
 end
